251a9a30c267e0ad660f20586547522468795138
lakhanrochwani <rochwanilakhan@gmail.com>
2022-01-09 00:32:59 -0600

    1 file modified

--- index.js
+++ index.js
@@ -1,32 +1,38 @@
-function addN(n) {
-  let sum = 0;
-  for (let i = 0; i <= n; i++) {
-    sum += i;
-  }
-  console.log('Inside', sum);
-  return sum;
-}
-console.time('time0');
-addN(50);
-console.timeEnd('time0');
-
 function mem_fn(fn) {
   let cache = [];
   return function (n) {
     let idx = n.toString();
     if (cache[idx] === undefined) {
-      console.log('index', idx, cache);
+      // console.log('index', idx, cache);
       cache[idx] = fn(n);
     }
-    console.log('Cached', cache[idx], cache);
+    // console.log('Cached', cache[idx], cache);
     return cache[idx];
   };
 }
-let mem = mem_fn(addN);
+
+function addN(n) {
+  let sum = 0;
+  for (let i = 0; i <= n; i++) {
+    sum += i;
+  }
+  // console.log('Inside', sum);
+  return sum;
+}
+// addN = mem_fn(addN);
+console.time('time0');
+addN(500000);
+console.timeEnd('time0');
+
 console.time('time1');
-mem(50);
+addN(500000);
 console.timeEnd('time1');
 
-console.time('time2');
-mem(50);
-console.timeEnd('time2');
+// let mem = mem_fn(addN);
+// console.time('time1');
+// mem(5000);
+// console.timeEnd('time1');
+
+// console.time('time2');
+// mem(5000);
+// console.timeEnd('time2');
